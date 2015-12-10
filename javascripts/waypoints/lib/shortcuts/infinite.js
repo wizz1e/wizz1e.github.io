314d6ac2caaaea0650631dcd3033e6009de45f19
wizz1e <mywifeyells@me.com>
2015-12-10 10:01:11 -0500

    pagination

--- javascripts/waypoints/lib/shortcuts/infinite.js
+++ javascripts/waypoints/lib/shortcuts/infinite.js
@@ -72,7 +72,7 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
   Infinite.defaults = {
     container: 'main col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2',
     items: '.article',
-    more: '[aria-label="Next"]',
+    more: '<a href="{{ pagination.next }}" ></a>',
     offset: 'bottom-in-view',
     loadingClass: 'infinite-loading',
     onBeforePageLoad: $.noop,
